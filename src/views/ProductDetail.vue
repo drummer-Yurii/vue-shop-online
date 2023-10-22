d8831b1acfac1ea5bbb60cd9e429e20c6da7e975
Yurii Rybachok <drummer2009@ukr.net>
2023-10-22 12:20:40 +0300

    render choise product in productDetails page

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,5 +1,14 @@
 <template>
-    <h1>Product details</h1>
+    <div class="product">
+        <div class="product-image">
+            <img :src="selectedProduct.thumbnail" alt="img" />
+        </div>
+        <div class="product-details">
+            <p>Brand: {{ selectedProduct.brand }}</p>
+            <p>Description: {{ selectedProduct.description }}</p>
+            <h2>Price: {{ selectedProduct.price }}</h2>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,9 +17,27 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'ProductDetails',
 })
+</script>
 
+<script setup>
+import {computed} from 'vue';
+import { productsStore } from '@/stores/products.js';
+import { useRoute } from 'vue-router';
+
+const store = productsStore();
+const route = useRoute();
+
+const selectedProduct = computed(() => {
+    return store.products.find((item) => item.id === Number(route.params.id));
+})
 </script>
 
 <style scoped>
-
+.product {
+    display: flex;
+    margin-top: 50px;
+}
+.product-image {
+    margin-right: 24px;
+}
 </style>
