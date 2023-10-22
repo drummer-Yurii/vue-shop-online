5816973323bef4b6df54d0df469bb756619fb2c4
Yurii Rybachok <drummer2009@ukr.net>
2023-10-22 11:45:25 +0300

    render products in catalog view

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,27 +1,37 @@
 <template>
-    <h1>Catalog</h1>
+  <div class="products-list">
+    <div 
+        class="product" 
+        v-for="product in store.products" 
+        :key="product.id"
+    >
+      <img :src="product.thumbnail" alt="img" />
+      <h2>Brand: {{ product.brand }}</h2>
+      <p>Description: {{ product.description }}</p>
+      <p>Price: ${{ product.price }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'CatalogView'
+  name: 'CatalogView'
 })
 </script>
 
 <script setup>
-import { onMounted } from 'vue';
-import { productsStore } from '@/stores/products';
+import { onMounted } from 'vue'
+import { productsStore } from '@/stores/products'
 
-const productStore = productsStore()
+const store = productsStore()
 
-onMounted(() => {
-    console.log('MOUNTED!!!!!!!!!!!');
-    productStore.fetchProductsFromDB()
+onMounted(async () => {
+    await store.fetchProductsFromDB();
+
+    console.log('MOUNTED!!!!!!!!!!!', store.products);
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
