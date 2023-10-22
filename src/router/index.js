5fc02cd40625b128ab8e2a48fafb44300b965f0c
Yurii Rybachok <drummer2009@ukr.net>
2023-10-22 11:32:23 +0300

    create catalog view & store for products

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Catalog from '@/views/Catalog.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      // component:
+      name: 'Catalog',
+      component: Catalog
     },
   ]
 })
---
+++ src/views/Catalog.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Catalog</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'CatalogView'
+})
+</script>
+
+<script setup>
+import { onMounted } from 'vue';
+import { productsStore } from '@/stores/products';
+
+const productStore = productsStore()
+
+onMounted(() => {
+    console.log('MOUNTED!!!!!!!!!!!');
+    productStore.fetchProductsFromDB()
+})
+</script>
+
+<style scoped>
+
+</style>
