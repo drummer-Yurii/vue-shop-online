a4561ebb980c62e3afb0a1f5772a618015480e7d
Yurii Rybachok <drummer2009@ukr.net>
2023-10-22 15:47:37 +0300

    show products in cart & delete

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Cart</h1>
+  <button @click="router.push({ name: 'Catalog' })" style="margin-bottom: 20px">Back to catalog</button>
+  <div v-if="!store.cart.length" style="text-align: center">
+    <h1>Empty Cart ...</h1>
+  </div>
+  <div class="cart-items" v-else>
+    <div class="cart-item" v-for="item in store.cart" :key="item.id">
+      <div class="item-details">
+        <img :src="item.thumbnail" alt="img" />
+        <span>Brand: {{ item.brand }}</span>
+        <span>Category: {{ item.category }}</span>
+        <span>Price: ${{ item.price }}</span>
+        <button @click="removeFromCart(item.id)">Remove</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +23,29 @@ export default defineComponent({
   name: 'CartView'
 })
 </script>
+<script setup>
+import { productsStore } from '../stores/products.js'
+import { useRouter } from 'vue-router'
 
-<style scoped></style>
+const store = productsStore()
+const router = useRouter()
+
+const removeFromCart = (id) => {
+  store.removeFromCart(id)
+}
+</script>
+
+<style scoped>
+.item-details {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 32px;
+  box-shadow: 0 0 17px 6px #e7e7e7;
+  border-radius: 8px;
+  padding: 16px;
+}
+.item-details img {
+  width: 20%;
+}
+</style>
